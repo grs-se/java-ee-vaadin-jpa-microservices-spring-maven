8547d38f9a44ef392803bfb867df44fb47263a37
grs-se <george_ricesmith@hotmail.com>
2024-06-16 10:28:14 +0100

    Second JPA package working

--- SecondJPA/src/main/java/com/udemy/inheritance/App.java
+++ SecondJPA/src/main/java/com/udemy/inheritance/App.java
@@ -1,10 +1,31 @@
 package com.udemy.inheritance;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		EntityManagerFactory factory = Persistence
+				.createEntityManagerFactory("com.grswebservices.jpa");
+		EntityManager entityManager = factory.createEntityManager();
+		
+		entityManager.getTransaction().begin();
+		
+		// make changes to the DB
+		// THERE ARE NO NULL VALUES (memory efficient)
+		// maybe it is harder to understand and comprehend
+		// different db tables for bus, car, vehicle, all linked via primary key
+		Bus bus = new Bus("Name of Bus", 100);
+		Car car = new Car("Name of Car", 150);
+		
+		entityManager.persist(bus);
+		entityManager.persist(car);
+		
+		entityManager.getTransaction().commit();
+		
+		entityManager.close();
+		factory.close();
 	}
-
 }
