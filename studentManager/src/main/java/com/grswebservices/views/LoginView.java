6da357c684125810be861976c72d875d0b798de7
grs-se <george_ricesmith@hotmail.com>
2024-07-31 11:15:20 +0100

    Login form implementation II

--- studentManager/src/main/java/com/grswebservices/views/LoginView.java
+++ studentManager/src/main/java/com/grswebservices/views/LoginView.java
@@ -1,5 +1,16 @@
 package com.grswebservices.views;
 
-public class LoginView {
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
 
+@PageTitle("Login")
+@Route(LoginView.PATH)
+public class LoginView extends VerticalLayout {
+	
+	public static final String PATH = "/login";
+	
+	public LoginView(LoginViewFactory loginViewFactory) {
+		add(loginViewFactory.create());
+	}
 }
