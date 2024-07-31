bf6e83807c76cec2c418f9c3ae8da8cb39b5b156
grs-se <george_ricesmith@hotmail.com>
2024-07-31 10:29:56 +0100

    Transactions introduction

--- studentManager/src/main/java/com/grswebservices/services/StatusServiceImpl.java
+++ studentManager/src/main/java/com/grswebservices/services/StatusServiceImpl.java
@@ -4,17 +4,23 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.grswebservices.model.Status;
 import com.grswebservices.repository.StatusRepository;
 
 @Service
+@Transactional(readOnly = true) // all methods of this class will inherit transactional behaviour
+// readonly = A boolean flag that can be set to true if the transaction iseffectively read-only, allowing for corresponding optimizations at runtime. 
 public class StatusServiceImpl implements StatusService {
 	
 	@Autowired
 	private StatusRepository statusRepository;
 
 	@Override
+	// default value is readOnly = false
+	// save method is not readonly because it changes values in the db
+	@Transactional
 	public void save(Status status) {
 		statusRepository.save(status);
 	}
